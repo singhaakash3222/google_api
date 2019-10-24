458bfe73609f5e3a75794e0fd1babb40a38d4bdb
Aakash Singha <aakash3222@gmail.com>
2019-10-24 17:24:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,40 @@
 	<title>Login With Google</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
+<style type="text/css">
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  cursor: pointer;
+ float: right;
+}
+
+
+.button3 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #f44336;
+}
+
+.button3:hover {
+  background-color: #f44336;
+  color: white;
+}
+
+
+</style>
 <body>
 <div class="container" style="margin-top: 100px">
+	<a href="logout.php" class="button button3" >Logout</a>
 	<div class="row">
 		<div class="col-md-3">
 			<img style="width: 80%;" src="<?php echo $_SESSION['picture'] ?>">
@@ -42,10 +74,7 @@
 						<td>Email</td>
 						<td><?php echo $_SESSION['email'] ?></td>
 					</tr>
-					<tr>
-						<td>Gender</td>
-						<td><?php echo $_SESSION['gender'] ?></td>
-					</tr>
+					
 				</tbody>
 			</table>
 		</div>
